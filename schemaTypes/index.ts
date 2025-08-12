d6ab959ef3407f6398b3a88d923e2a5ce8ef03a9
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-08-12 11:50:39 +0300

    Add files via upload

---
+++ schemaTypes/index.ts
@@ -0,0 +1,7 @@
+// schemas/index.ts
+import user from './user';
+import category from './category';
+import collection from './collections';
+import product from './product';
+
+export const schemaTypes = [user, category, product, collection];
