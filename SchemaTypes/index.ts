0a9ef6abf3f243fb846b49b4e6767e3e451ec65d
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-08-12 11:49:51 +0300

    Delete SchemaTypes directory

--- SchemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-// schemas/index.ts
-import user from './user';
-import category from './category';
-import collection from './collections';
-import product from './product';
-
-export const schemaTypes = [user, category, product, collection];
